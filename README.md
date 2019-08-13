709bbba3f8ce35c150db1c64864ce4b9e6688fba
eamiear <justcall_me_k@163.com>
2019-08-13 14:38:23 +0800

    ''

---
+++ README.md
@@ -0,0 +1,32 @@
+# obicon
+
+iconfont of on-bright
+
+通用字体图标，整合于iconfont
+
+## 结构
+
+```text
+|++ docs
+|++ src
+|++ source
+|- build.js
+```
+
+| 文件   | 描述                   |
+| ------ | ---------------------- |
+| docs   | 在线使用说明文档       |
+| src    | 源文件                 |
+| source | 从iconfont导下来的文件 |
+
+## 构建
+
+```shell
+npm run build
+```
+
+## 部署
+
+```shell
+npm run deploy
+```
---
+++ build.js
@@ -0,0 +1,57 @@
+const cheerio = require('cheerio')
+const fs = require('fs')
+const path = require('path')
+const ICONNAME = 'obicon'
+
+function dataCleaning () {
+  let source = fs.readdirSync('source')
+  source.forEach((item, index) => {
+    const filePath = path.join('source', item)
+    if (item.includes('iconfont')) {
+      const fileSuffix = filePath.slice(filePath.indexOf('.'))
+      fs.renameSync(filePath, path.join('source', `${ICONNAME}${fileSuffix}`))
+    }
+    if (item.includes('demo_')) {
+      fs.renameSync(filePath, path.join('source', 'index.html'))
+    }
+  })
+  let data = fs.readFileSync(path.join('source', 'index.html'), 'utf8')
+  data = data.replace(/iconfont/g, ICONNAME)
+  const $ = cheerio.load(data)
+  $('title').text(`${ICONNAME} Demo`)
+  $('[type="image/x-icon"]').remove()
+  $('.logo > a').attr('href', '#').attr('target', '_self').text(ICONNAME)
+  $('.nav-more').remove()
+  data = $.html()
+  fs.writeFileSync(path.join('source', 'index.html'), data)
+}
+
+function cleardir (dir) {
+  Array.from(fs.readdirSync(dir)).forEach(file => {
+    const filePath = path.join(dir, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isFile()) {
+      fs.unlinkSync(filePath)
+    }
+  })
+}
+
+function proccessor () {
+  cleardir(path.resolve(__dirname, 'docs'))
+  cleardir(path.resolve(__dirname, 'src'))
+  fs.readdirSync('source').forEach(file => {
+    fs.copyFileSync(path.join('source', file), path.join('docs', file))
+  })
+  fs.readdirSync('source').forEach(file => {
+    if (['.css', '.eot', '.svg', '.ttf', '.woff', '.woff2'].includes(file.slice(file.indexOf('.'))) && file.indexOf('demo') === -1) {
+      fs.copyFileSync(path.join('source', file), path.join('src', file))
+    }
+  })
+}
+
+function main () {
+  dataCleaning()
+  proccessor()
+}
+
+main()
